38235f3f5b767d1e63a9873784c3cda2e8549619
Your_Name <joseph161216@gmail.com>
2020-04-08 19:05:21 -0700

    create PostSchema

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
 router.get("/posts", (req, res) => {
   res.send("we are on posts");
