81df437cacc4eaa2294c55d4b094a841e061035d
Your_Name <joseph161216@gmail.com>
2020-04-08 18:46:41 -0700

    connect to database and separate routes

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Import Routes
+const postsRoute = require("./routes/posts")
+
+app.use("/posts", postsRoute);
+
+
+// //MIDDLEWARE
+// app.use("/posts", () => {
+//   console.log("posts middleware was hit");
+// })
 
 //ROUTES
 app.get("/", (req, res) => {
@@ -10,6 +23,12 @@ app.get("/posts", (req, res) => {
   res.send("we are on posts");
 })
 
+//Connect to DB
+mongoose.connect("process.env.MONGO_URI", { useNewUrlParser: true }, () => {
+  console.log("Connected to DB!!!!")
+});
+
+//Listen to server
 app.listen(3000, () => {
   console.log("Server is listening on port 3000...")
 })
